13844a05cf5c2a85adfd29de233c51019378625b
Viacheslav Tykhovlis <front.tykhovlis@gmail.com>
2024-06-03 21:15:31 +0200

    Task 05
    Add task 5

---
+++ js/task-05.js
@@ -0,0 +1,32 @@
+function createPhoneNumber(numbers) {
+  const tel =
+    "(" +
+    numbers[0] +
+    numbers[1] +
+    numbers[2] +
+    ")" +
+    " " +
+    numbers[3] +
+    numbers[4] +
+    numbers[5] +
+    "-" +
+    numbers[6] +
+    numbers[7] +
+    numbers[8] +
+    numbers[9];
+
+  return tel;
+}
+
+console.log(
+  "createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) ",
+  createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+);
+console.log(
+  "createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) ",
+  createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+);
+console.log(
+  "createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) ",
+  createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+);
